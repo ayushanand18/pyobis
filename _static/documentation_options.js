26f872937798a99c2df6ac8d5b9aeff07b7c8c86
ayushanand18 <ayushanand18@users.noreply.github.com>
2022-11-22 12:04:47 +0000

    deploy: 4eb3820f37cbbc7e3270a4597020ef1b1a7efb5b

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.1.dev356+g9d6b949',
+    VERSION: '0.1.1.dev376+g4eb3820',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
