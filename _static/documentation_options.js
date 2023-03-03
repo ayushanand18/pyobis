23df5bba265a88e71e649e9d80eb8de749c306f6
ayushanand18 <ayushanand18@users.noreply.github.com>
2023-03-03 19:38:36 +0000

    deploy: 498ab8872cb1f4602432880806a79fb344ae0b9f

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.1.dev385+gf641712',
+    VERSION: '0.1.1.dev387+g498ab88',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
